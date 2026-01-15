44248ad50a92eb63d5164b1329593f72ce210c02
Bùi Thắng <buithang1410@gmail.com>
2026-01-15 08:54:57 +0700

    Update config.sample.php

--- config.sample.php
+++ config.sample.php
@@ -2,14 +2,16 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'simplecms');
 define('DB_USER', 'simplecms_user');
-define('DB_PASS', 'password');
+define('DB_PASS', 'MAT_KHAU_DB_CUA_BAN');
 
-define('SITE_URL', 'http://localhost');
+define('SITE_URL', 'http://192.168.202.134');
 
-define('AUTH_KEY', 'changeme');
-define('SECURE_AUTH_KEY', 'changeme');
-
-session_start();
+define('AUTH_KEY', 'key123');
+define('SECURE_AUTH_KEY', 'key456');
