9434e86a60bd42c1dae768ff27bf0d70139d4738
Bùi Thắng <buithang1410@gmail.com>
2026-01-15 08:36:48 +0700

    Update config.sample.php

--- config.sample.php
+++ config.sample.php
@@ -3,9 +3,10 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'simplecms');
 define('DB_USER', 'simplecms_user');
 define('DB_PASS', 'password');
+
 define('SITE_URL', 'http://localhost');
 
-// Các khóa bảo mật (Script sẽ tự thay thế bằng chuỗi ngẫu nhiên)
-define('AUTH_KEY', 'put-your-random-key-here');
-define('SECURE_AUTH_KEY', 'put-your-random-key-here');
-?>
+define('AUTH_KEY', 'changeme');
+define('SECURE_AUTH_KEY', 'changeme');
+
+session_start();
