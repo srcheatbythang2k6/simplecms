7673b3becef938007544b9b3997565991f9fe44a
Bùi Thắng <buithang1410@gmail.com>
2026-01-15 08:25:39 +0700

    Delete admin/database.sql

--- admin/database.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `posts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `content` text NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
