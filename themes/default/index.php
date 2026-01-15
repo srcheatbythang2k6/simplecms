0c24684ef194bc3a9e398f0a0c82b69e16f4d333
Bùi Thắng <buithang1410@gmail.com>
2026-01-15 08:40:58 +0700

    Update index.php

--- themes/default/index.php
+++ themes/default/index.php
@@ -1,1 +1,9 @@
+<?php require 'header.php'; ?>
 
+<?php foreach ($posts as $post): ?>
+    <h2><?= e($post['title']) ?></h2>
+    <p><?= nl2br(e($post['content'])) ?></p>
+    <hr>
+<?php endforeach; ?>
+
+<?php require 'footer.php'; ?>
