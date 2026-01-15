698ea6de3e124fe1d1865483256e4de3a22d885c
Bùi Thắng <buithang1410@gmail.com>
2026-01-15 09:09:16 +0700

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -5,9 +5,9 @@ try {
     $pdo = new PDO(
         "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4",
         DB_USER,
-        DB_PASS,
-        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+        DB_PASS
     );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("DB ERROR: " . $e->getMessage());
+    die("❌ DB ERROR: " . $e->getMessage());
 }
